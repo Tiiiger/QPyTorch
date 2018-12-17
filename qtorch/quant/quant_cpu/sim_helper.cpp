9bb7cafb91c5d1b0f5325375c6c5255926a12489
Tiiiger <zty27x@gmail.com>
2018-12-17 12:43:56 -0500

    change modularity for quant cpu.

--- qtorch/quant/quant_cpu/sim_helper.cpp
+++ qtorch/quant/quant_cpu/sim_helper.cpp
@@ -2,28 +2,6 @@
 #include <math.h>
 #include <stdint.h>
 
-
-
-template <typename T>
-T clamp_helper(T a, T min, T max) {
-  if (a > max) return max;
-  else if (a < min) return min;
-  else return a;
-}
-
-template <typename T>
-T clamp_mask_helper(T a, T min, T max, uint8_t* mask) {
-  if (a > max) {
-    *mask = 1;
-    return max;
-  }
-  else if (a < min) {
-    *mask = 1;
-    return min;
-  }
-  else return a;
-}
-
 void fixed_min_max(int wl, int fl, bool symmetric, float* t_min, float* t_max) {
   int sigma = -fl;
   *t_min = -ldexp(1.0, wl-fl-1);
