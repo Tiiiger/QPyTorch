0e186784fabf129c6c83b243bdb36f58e7f70a76
Tiiiger <zty27x@gmail.com>
2018-12-18 15:17:51 -0500

    move weight acc into optim.

--- qtorch/optim/sgd_low.py
+++ qtorch/optim/sgd_low.py
@@ -9,7 +9,8 @@ class SGDLP(Optimizer):
                  grad_scaling=1,
                  weight_quant=None,
                  grad_quant=None,
-                 momentum_quant=None):
+                 momentum_quant=None,
+                 acc_quant=None):
         assert isinstance(sgd_optim, SGD)
         super(SGDLP, self).__init__(sgd_optim.param_groups, sgd_optim.defaults) # place holder
 
@@ -24,20 +25,38 @@ class SGDLP(Optimizer):
         self.grad_quant=grad_quant
         self.momentum_quant=momentum_quant
 
+        if self.acc_quant != None:
+            self.weight_acc = {}
+            for group in self.param_groups:
+                for p in group['params']:
+                    self.weight_acc[p] = p.detach().clone()
+
     def step(self, closure=None):
         # quantize gradient
-        for group in self.param_groups:
-            for p in group['params']:
-                if not self.grad_quant is None:
+        if not self.grad_quant is None:
+            for group in self.param_groups:
+                for p in group['params']:
                     p.grad.data = self.grad_quant(p.grad.data*self.grad_scaling)
+
+        # switch acc into weight before stepping
+        if not self.acc_quant is None:
+            for group in self.param_groups:
+                for p in group['params']:
+                    p.data = self.weight_acc[p].data
 
         loss = self.sgd_optim.step()
 
-        # quantize weight
+        # switch weight into acc after stepping and quantize
+        if not self.acc_quant is None:
+            for group in self.param_groups:
+                for p in group['params']:
+                    p.data = self.weight_acc[p].data = self.acc_quant(p.data).data
+
+        # quantize weight from acc
         if not self.weight_quant is None:
             for group in self.param_groups:
                 for p in group['params']:
-                    p.data = self.weight_quant(p.data)
+                    p.data = self.weight_quant(p.data).data
 
         # quantize momentum
         if not self.momentum_quant is None:
