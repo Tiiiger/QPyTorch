5e9abdbc8e1216b837946c535242496c7b94a168
Tiiiger <zty27x@gmail.com>
2019-02-18 01:03:48 -0500

    adding more documentations on low-precision numbers.

--- qtorch/number.py
+++ qtorch/number.py
@@ -8,13 +8,51 @@ class Number():
         raise NotImplemented
 
 class FixedPoint(Number):
-    """
-    Low-Precision Fixed Point Format.
+    r"""
+    Low-Precision Fixed Point Format. Defined similarly in
+    *Deep Learning with Limited Numerical Precision* (https://arxiv.org/abs/1502.02551)
+
+    The representable range is :math:`[-2^{wl-fl-1}, 2^{wl-fl-1}-2^{-fl}]`
+    and a precision unit (smallest nonzero absolute value) is
+    :math:`2^{-fl}`.
+    Numbers outside of the representable range can be clamped
+    (if `clamp` is true).
+    We can also give up the smallest representable number to make the range
+    symmetric, :math:`[-2^{wl-fl-1}^{-fl}, 2^{wl-fl-1}-2^{-fl}]`. (if `symmetric` is true).
+
+    Define :math:`\lfloor x \rfloor` to be the largest representable number (multiples of :math:`2^{-fl}`) smaller than :math:`x`.
+    For numbers within the representable range, fixed point quantizatio corresponds to
+
+    .. math::
+
+       NearestRound(x)
+       =
+       \Biggl \lbrace
+       {
+       \lfloor x \rfloor, \text{ if } \lfloor x \rfloor \leq x \leq \lfloor x \rfloor + 2^{-fl-1}
+       \atop
+        \lfloor x \rfloor + 2^{-fl}, \text{ if } \lfloor x \rfloor + 2^{-fl-1} < x \leq \lfloor x \rfloor + 2^{-fl}
+       }
+
+    or
+
+    .. math::
+       StochasticRound(x)
+       =
+       \Biggl \lbrace
+       {
+       \lfloor x \rfloor, \text{ with probabilty } 1 - \frac{x - \lfloor x \rfloor}{2^{-fl}}
+       \atop
+        \lfloor x \rfloor + 2^{-fl}, \text{ with probabilty } \frac{x - \lfloor x \rfloor}{2^{-fl}}
+       }
 
     Args:
-        - :attr: `wl` word length of each fixed point number
-        - :attr: `fl` fractional length of each fixed point number
+        - :attr: wl (int) : word length of each fixed point number
+        - :attr: fl (int) : fractional length of each fixed point number
+        - :attr: clamp (bool) : whether to clamp unrepresentable numbers
+        - :attr: symmetric (bool) : whether to make the representable range symmetric
     """
+
     def __init__(self, wl, fl, clamp=True, symmetric=False):
         assert wl > 0, "invalid bits for word length: {}".format(wl)
         assert fl > 0, "invalid bits for fractional length: {}".format(fl)
@@ -26,29 +64,18 @@ class FixedPoint(Number):
         self.symmetric = symmetric
 
     def __str__(self):
-        return "FixedPoint Number with wl={:d}, fl={:d}".format(self.wl, self.fl)
-
-class BlockFloatingPoint(Number):
-    """
-    Low-Precision Block Floating Point Format.
-    Currently only supports treating the entire input tensor as a block.
-
-    Args:
-        - :attr: `wl` word length of the tensor
-    """
-    def __init__(self, wl):
-        assert wl > 0, "invalid bits for word length:{}".format(wl)
-        self.wl = wl
-
-    def __str__(self):
-        return "BlockFloatingPoint Number with wl={:d}".format(self.wl)
+        return "FixedPoint (wl={:d}, fl={:d})".format(self.wl, self.fl)
 
     def __repr__(self):
-        return "BlockFloatingPoint Number with wl={:d}".format(self.wl)
+        return "FixedPoint (wl={:d}, fl={:d})".format(self.wl, self.fl)
 
 class FloatingPoint(Number):
     """
-    Low-Precision Floating Point Format
+    Low-Precision Floating Point Format.
+
+    We set the exponent bias to be :math:`2^{exp-1}`. In our simulation, we do
+    not handle denormal/subnormal numbers and infinities/NaNs. For rounding
+    mode, we apply *round to nearest even*.
 
     Args:
         - :attr: `exp`: number of bits allocated for exponent
@@ -62,7 +89,29 @@ class FloatingPoint(Number):
         self.man = man
 
     def __str__(self):
-        return "FloatingPoint Number with exponent={:d}, mantissa={:d}".format(self.exp, self.man)
+        return "FloatingPoint (exponent={:d}, mantissa={:d})".format(self.exp, self.man)
 
     def __repr__(self):
-        return "FloatingPoint Number with exponent={:d}, mantissa={:d}".format(self.exp, self.man)
+        return "FloatingPoint (exponent={:d}, mantissa={:d})".format(self.exp, self.man)
+
+class BlockFloatingPoint(Number):
+    """
+    Low-Precision Block Floating Point Format.
+
+    BlockFloatingPoint shares an exponent across a block of numbers. The shared exponent is chosen from
+    the largest magnitude in the block.
+
+    Currently our implementation only supports treating the entire input tensor as a block.
+
+    Args:
+        - :attr: `wl` word length of the tensor
+    """
+    def __init__(self, wl):
+        assert wl > 0, "invalid bits for word length:{}".format(wl)
+        self.wl = wl
+
+    def __str__(self):
+        return "BlockFloatingPoint (wl={:d})".format(self.wl)
+
+    def __repr__(self):
+        return "BlockFloatingPoint (wl={:d})".format(self.wl)
