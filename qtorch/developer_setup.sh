f03cf0ae039bb9e8337358c2c620973dd4bb3e38
Tiiiger <zty27x@gmail.com>
2019-04-30 12:13:17 -0400

    remove outdated files.

--- qtorch/developer_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/expect
-spawn {pip uninstall qtorch-cpu qtorch-cuda}
-expect "?"
-send "y\n"
-expect "?"
-send "y\n"
-send "python quant/quant-cpu/setup.py install \n"
-send "python quant/quant-cuda/setup.py install \n"
