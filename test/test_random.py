a53839c3fa44205d8c7d4823dba259ade51878cd
Tiiiger <zty27x@gmail.com>
2020-04-26 02:12:45 -0400

    include random test for cuda quantization.

--- test/test_random.py
+++ test/test_random.py
@@ -13,16 +13,15 @@ class TestQuant(unittest.TestCase):
         S = lambda bits: 2 ** (bits)
         Q = lambda x, bits: torch.round(x * S(bits)) / S(bits)
         wl = 8
-        quant = lambda x: fixed_point_quantize(
-            x, wl=wl, fl=wl, clamp=False, rounding="nearest"
-        )
+        quant = lambda x: fixed_point_quantize(x, wl=wl, fl=wl, clamp=False, rounding="nearest")
 
         N = int(1e8)
-        x = torch.randn(N, device="cuda")
-        oracle = Q(x, wl)
-        target = quant(x)
-        matched = torch.eq(oracle, target).all().item()
-        self.assertTrue(matched)
+        for device in ["cpu", "cuda"]:
+            x = torch.randn(N, device="cpu")
+            oracle = Q(x, wl)
+            target = quant(x)
+            matched = torch.eq(oracle, target).all().item()
+            self.assertTrue(matched)
 
 
 if __name__ == "__main__":
