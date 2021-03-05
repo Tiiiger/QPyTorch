d94f1dc46bf3475b2226f695913b032f23533fe3
Daniel Holanda Noronha <danielholandanoronha@Daniels-MacBook-Pro-2.local>
2021-03-05 14:43:01 -0800

    Fixed nearest float quantization

--- qtorch/quant/quant_cpu/bit_helper.cpp
+++ qtorch/quant/quant_cpu/bit_helper.cpp
@@ -1,4 +1,5 @@
 #include "quant_cpu.h"
+#include "stdio.h"
 
 unsigned int clip_exponent(int exp_bits, int man_bits,
                            unsigned int old_num,
@@ -8,29 +9,14 @@ unsigned int clip_exponent(int exp_bits, int man_bits,
     return quantized_num;
 
   int quantized_exponent_store = quantized_num << 1 >> 1 >> 23; // 1 sign bit, 23 mantissa bits
-  int min_exponent_store = -((1 << (exp_bits - 1)) - 1) + 127;
-  int max_exponent_store = ((1 << (exp_bits - 1)) - 1) + 127; // excluding the exponent for infinity
+  int max_exponent_store = (1 << (exp_bits - 1)) + 127; // we are not reserving an exponent bit for infinity, nan, etc
+  // Clippping Value Up
   if (quantized_exponent_store > max_exponent_store)
   {
     unsigned int max_man = (unsigned int)-1 << 9 >> 9 >> (23 - man_bits) << (23 - man_bits); // 1 sign bit, 8 exponent bits, 1 virtual bit
     unsigned int max_num = ((unsigned int)max_exponent_store << 23) | max_man;
     unsigned int old_sign = old_num >> 31 << 31;
     quantized_num = old_sign | max_num;
-  }
-  else if (quantized_exponent_store < min_exponent_store)
-  {
-    unsigned int min_num = ((unsigned int)min_exponent_store << 23);
-    unsigned int middle_num = ((unsigned int)(min_exponent_store - 1) << 23);
-    unsigned int unsigned_quantized_num = quantized_num << 1 >> 1;
-    if (unsigned_quantized_num > middle_num)
-    {
-      unsigned int old_sign = old_num >> 31 << 31;
-      quantized_num = old_sign | min_num;
-    }
-    else
-    {
-      quantized_num = 0;
-    }
   }
   return quantized_num;
 }
@@ -49,8 +35,3 @@ unsigned int clip_max_exponent(int man_bits,
   }
   return quantized_num;
 }
-// unsigned int extract_exponent(float *a) {
-//   unsigned int temp = *(reinterpret_cast<unsigned int*>(a));
-//   temp = (temp << 1 >> 24); // single precision, 1 sign bit, 23 mantissa bits
-//   return temp-127+1; // exponent offset and virtual bit
-// }
