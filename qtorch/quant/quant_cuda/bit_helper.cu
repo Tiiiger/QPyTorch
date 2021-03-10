1b8ca0b46d9848140bd29dbce9812704e4a7fb5a
Daniel Holanda Noronha <danielhn@beluga3.int.ets1.calculquebec.ca>
2021-03-09 21:27:30 -0500

    Fixed float quantization for cuda

--- qtorch/quant/quant_cuda/bit_helper.cu
+++ qtorch/quant/quant_cuda/bit_helper.cu
@@ -28,27 +28,18 @@ __device__ __forceinline__ unsigned int round_bitwise_nearest(unsigned int targe
 __device__ __forceinline__ unsigned int clip_exponent(int exp_bits, int man_bits,
                                                       unsigned int old_num,
                                                       unsigned int quantized_num) {
+  if (quantized_num == 0)
+    return quantized_num;
+
   int quantized_exponent_store = quantized_num << 1 >> 1 >> 23; // 1 sign bit, 23 mantissa bits
-  int min_exponent_store = -((1 << (exp_bits-1))-1) + 127;
-  int max_exponent_store = ((1 << (exp_bits-1))-1) + 127; // excluding the exponent for infinity
-  if (quantized_exponent_store > max_exponent_store) {
-    unsigned int max_man = (unsigned int ) -1 << 9 >> 9 >> (23-man_bits) << (23-man_bits); // 1 sign bit, 8 exponent bits, 1 virtual bit
-    unsigned int max_num = ((unsigned int) max_exponent_store << 23) | max_man;
+  int max_exponent_store = (1 << (exp_bits - 1)) + 127; // we are not reserving an exponent bit for infinity, nan, etc
+  // Clippping Value Up
+  if (quantized_exponent_store > max_exponent_store)
+  {
+    unsigned int max_man = (unsigned int)-1 << 9 >> 9 >> (23 - man_bits) << (23 - man_bits); // 1 sign bit, 8 exponent bits, 1 virtual bit
+    unsigned int max_num = ((unsigned int)max_exponent_store << 23) | max_man;
     unsigned int old_sign = old_num >> 31 << 31;
     quantized_num = old_sign | max_num;
-  } else if (quantized_exponent_store < min_exponent_store) {
-    unsigned int min_num = ((unsigned int)min_exponent_store << 23);
-    unsigned int middle_num = ((unsigned int)(min_exponent_store - 1) << 23);
-    unsigned int unsigned_quantized_num = quantized_num << 1 >> 1;
-    if (unsigned_quantized_num > middle_num)
-    {
-      unsigned int old_sign = old_num >> 31 << 31;
-      quantized_num = old_sign | min_num;
-    }
-    else
-    {
-      quantized_num = 0;
-    }
   }
   return quantized_num;
 }
--- qtorch/quant/quant_cuda/float_kernel.cu
+++ qtorch/quant/quant_cuda/float_kernel.cu
@@ -10,12 +10,35 @@ __global__ void float_kernel_stochastic(float* __restrict__ a,
                                         int exp_bits) {
   int index = blockIdx.x * blockDim.x + threadIdx.x;
   if (index < size) {
-    unsigned int old_num = FLOAT_TO_BITS(&a[index]);
+    //unsigned int old_num = FLOAT_TO_BITS(&a[index]);
+    //unsigned int rand_prob = (unsigned int) r[index];
+    //unsigned int quantize = round_bitwise_stochastic(old_num, rand_prob, man_bits);
+    //quantize = clip_exponent(exp_bits, man_bits, old_num, quantize);
+    //float quantize_float = BITS_TO_FLOAT(&quantize);
+    //o[index] = quantize_float;
     unsigned int rand_prob = (unsigned int) r[index];
-    unsigned int quantize = round_bitwise_stochastic(old_num, rand_prob, man_bits);
-    quantize = clip_exponent(exp_bits, man_bits, old_num, quantize);
-    float quantize_float = BITS_TO_FLOAT(&quantize);
-    o[index] = quantize_float;
+    unsigned int target,quantize_bits;
+    target = FLOAT_TO_BITS(&a[index]);
+    float quantized;
+
+    int target_exp = (target << 1 >> 1 >> 23) -127; 
+    int min_exp = -((1 << (exp_bits - 1)) - 2);
+    bool subnormal = (target_exp < min_exp);
+    if (subnormal){
+      float shift_float,val;
+      int shift_bits = ((127+min_exp)<<23) | (target >> 31 <<31);
+      shift_float = BITS_TO_FLOAT(&shift_bits);
+      val=a[index]+shift_float;
+      target = FLOAT_TO_BITS(&val);
+      quantize_bits = round_bitwise_stochastic(target, rand_prob, man_bits);
+      quantized = BITS_TO_FLOAT(&quantize_bits) - shift_float;
+    }
+    else{
+      quantize_bits = round_bitwise_stochastic(target, rand_prob, man_bits);
+      quantize_bits = clip_exponent(exp_bits, man_bits, target, quantize_bits);
+      quantized = BITS_TO_FLOAT(&quantize_bits);
+    }
+    o[index] = quantized;
   }
 }
 
@@ -27,10 +50,33 @@ __global__ void float_kernel_nearest(float* __restrict__ a,
                                      int exp_bits) {
   int index = blockIdx.x * blockDim.x + threadIdx.x;
   if (index < size) {
-    unsigned int old_num = FLOAT_TO_BITS(&a[index]);
-    unsigned int quantize = round_bitwise_nearest(old_num, man_bits);
-    quantize = clip_exponent(exp_bits, man_bits, old_num, quantize);
-    float quantize_float = BITS_TO_FLOAT(&quantize);
-    o[index] = quantize_float;
+
+    //unsigned int old_num = FLOAT_TO_BITS(&a[index]);
+    //unsigned int quantize = round_bitwise_nearest(old_num, man_bits);
+    //quantize = clip_exponent(exp_bits, man_bits, old_num, quantize);
+    //float quantize_float = BITS_TO_FLOAT(&quantize);
+    //o[index] = quantize_float;
+    unsigned int target,quantize_bits;
+    target = FLOAT_TO_BITS(&a[index]);
+    float quantized;
+
+    int target_exp = (target << 1 >> 1 >> 23) -127; 
+    int min_exp = -((1 << (exp_bits - 1)) - 2);
+    bool subnormal = (target_exp < min_exp);
+    if (subnormal){
+      float shift_float,val;
+      int shift_bits = ((127+min_exp)<<23) | (target >> 31 <<31);
+      shift_float = BITS_TO_FLOAT(&shift_bits);
+      val=a[index]+shift_float;
+      target = FLOAT_TO_BITS(&val);
+      quantize_bits = round_bitwise_nearest(target, man_bits);
+      quantized = BITS_TO_FLOAT(&quantize_bits) - shift_float;
+    }
+    else{
+      quantize_bits = round_bitwise_nearest(target, man_bits);
+      quantize_bits = clip_exponent(exp_bits, man_bits, target, quantize_bits);
+      quantized = BITS_TO_FLOAT(&quantize_bits);
+    }
+    o[index] = quantized;
   }
 }
