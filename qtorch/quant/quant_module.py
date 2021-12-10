Add Dynamic-precision Quantization to fixed precision
- This Quantization is based on https://github.com/arkainoh/fixedpoint/blob/master/src/cpp/fixedpoint.h
- Different quantization is applied for each layer
  - Dynamic fractional length for each layer
- Set 'dynamic_precision' as True for use
  - example)
    Q = Quantizer(forward_number=forward_num,
      forward_rounding="stochastic",
      dynamic_precision=True)

Signed-off-by: LIM SUNGWON <sungwon.lim@neubla.com>

qtorch/quant/quant_module.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+from qtorch import BlockFloatingPoint, FloatingPoint
 from .quant_function import *
 import numpy as np
 
@@ -13,10 +14,15 @@ class Quantizer(nn.Module):
         backward_number=None,
         forward_rounding="stochastic",
         backward_rounding="stochastic",
+        dynamic_precision=False,
     ):
+        if dynamic_precision == True:
+            if type(forward_number) == FloatingPoint or type(forward_number) == BlockFloatingPoint:
+                print("Not support DYNAMIC PRECISION in Floating point")
+
         super(Quantizer, self).__init__()
         self.quantize = quantizer(
-            forward_number, backward_number, forward_rounding, backward_rounding
+            forward_number, backward_number, forward_rounding, backward_rounding, dynamic_precision,
         )
 
     def forward(self, x):